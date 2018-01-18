3b59ec94b34609c7c511cdd51a3b61da7137f4c8
Brandon James <bjames9076@gmail.com>
2018-01-18 18:50:41 -0500

    added a basic friend

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+   
+  {
+    "name":"Bob",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+       5,
+       1,
+       4,
+       3,
+       2,
+       6,
+       9,
+       8,
+       9,
+       9
+        ]
+  }
+  ]
+  
+  module.exports = friends;
