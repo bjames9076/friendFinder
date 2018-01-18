8f7a97de87dfa4ed9ad2d92ed26a7d6aa09d4dba
Brandon James <bjames9076@gmail.com>
2018-01-18 18:41:20 -0500

    added html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+module.exports = function(app){
+        //route to survey
+        app.get('/survey', function(req, res){
+            res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        });
+        //route to home
+        app.use(function(req, res){
+            res.sendFile(path.join(__dirname + '/../public/home.html'));
+        });
+    
+    }
