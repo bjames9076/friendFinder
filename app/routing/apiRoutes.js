03c0543293c69c0ca70e4745d621f0d924dd510d
Brandon James <bjames9076@gmail.com>
2018-01-18 18:44:12 -0500

    added api/friends path

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var friendData = require('../data/friends.js');
+var path = require('path');
+
+
+
+var totalDifference = 0;
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+    });
+    
+};
