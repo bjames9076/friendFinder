b7dd05e8571e4a58ecfb7086517c97799ad30676
Brandon James <bjames9076@gmail.com>
2018-01-19 01:10:38 -0500

    trying to figure out deployment issue

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(express.static(__dirname + '/app/public'));
 
 require('./app/routing/apiroutes.js')(app); 
 require('./app/routing/htmlroutes.js')(app);
